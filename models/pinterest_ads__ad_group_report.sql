c760ec7464e8e636379702953585243eb5bed648
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2023-12-19 19:52:58 +0530

    Update pinterest_ads__ad_group_report.sql

--- models/pinterest_ads__ad_group_report.sql
+++ models/pinterest_ads__ad_group_report.sql
@@ -1,4 +1,5 @@
 {{ config(enabled=var('ad_reporting__pinterest_ads_enabled', True),
+    unique_key = ['source_relation','ad_group_id','advertiser_id','campaign_id','date_day'],
     partition_by={
       "field": "date_day", 
       "data_type": "TIMESTAMP",
